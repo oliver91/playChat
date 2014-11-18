8f14ec1c51512ada4ba3520a1e0b0741f1979df6
oliver91 <oliver91@ukr.net>
2014-11-18 18:30:19 +0200

    Initial commit

---
+++ app/controllers/Application.java
@@ -0,0 +1,36 @@
+
+package controllers;
+
+import models.SimpleChat;
+import play.mvc.Controller;
+import play.mvc.Result;
+import play.mvc.WebSocket;
+import views.html.index;
+import assets.js.ws;
+
+public class Application extends Controller {
+
+    // render index page
+    public static Result index()
+    {
+        return ok(index.render());
+    }
+
+    // get the ws.js script
+    public static Result wsJs()
+    {
+        return ok(ws.render());
+    }
+
+    // Websocket interface
+    public static WebSocket<String> wsInterface()
+    {
+        return new WebSocket<String>()
+        {
+            // called when websocket handshake is done
+            public void onReady(WebSocket.In<String> in, WebSocket.Out<String> out){
+                SimpleChat.start(in, out);
+            }
+        };
+    }
+}
